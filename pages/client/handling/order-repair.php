5b74596462bbbeeb6f7d66cd4c24afda70cbde2b
Kyriake47 <129898211+Kyriake47@users.noreply.github.com>
2024-07-09 23:33:05 +0200

    windows

---
+++ pages/client/handling/order-repair.php
@@ -0,0 +1,5 @@
+<?php 
+    include("../../config/connection.php");
+    session_start();
+
+?>
---
+++ pages/client/windows/order-repair.php
@@ -0,0 +1,12 @@
+<div class="modal-header">
+    <h5 class="modal-title" id="dynamicModalLabel">Tytuł Okienka 1</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<div class="modal-body">
+    <p>To jest zawartość okienka 1.</p>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+</div>
